1d986a01eacd5c9c5bbdf66dbf1ccd2575a993c1
Fatih Demir <f.demir@avisi.nl>
2017-05-29 17:17:42 +0200

    Change profiles.

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,2 @@
-{:user {:dependencies [[refactor-nrepl "2.3.0-SNAPSHOT"]
-                       [cider/cider-nrepl "0.14.0"]
-                       [lein-deps-tree "0.1.2"] ]
-        :plugins [[lein-ancient "0.6.10"]
-                  [lein-codeindex "0.1.0"]
-                  [lein-bikeshed "0.4.1"]
-                  [lein-kibit "0.1.5"]
-                  ]}}
+{:user {:plugins [[cider/cider-nrepl "0.14.0"]]
+        :repl-options {:timeout 120000}}}
